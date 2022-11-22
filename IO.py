a8d6b91abe67c0bf12d8ee311b5637ec62c8eea6
Vitor-Spader <vitor.h.spader@gmail.com>
2022-11-21 22:41:25 -0300

    review

--- IO.py
+++ IO.py
@@ -1,26 +1,44 @@
 import RPi.GPIO as GPIO
-import time
 
-def start():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(17, GPIO.OUT)
-    GPIO.setup(2, GPIO.IN)
-    return (True if GPIO.input(2) == 0 else False)
+class IO:
+    def __init__(self,mode='BCM',l_inp=[],l_out=[]):
+        self.mode = mode
+        self.l_inp = l_inp
+        self.l_out = l_out
 
-def verif_in(num):
-    return (True if GPIO.input(num) == 0 else False) 
+        if mode == 'BCM': GPIO.setmode(GPIO.BCM)
+        else: GPIO.setmode(GPIO.BOARD)
+        self.setup(self)
+    
+    def setup(self):
+        for x in self.l_inp:
+            GPIO.setup(x,GPIO.IN)
+        for x in self.l_out:
+            GPIO.setup(x,GPIO.OUT)
+    
+    def verif_in(inp):
+        return GPIO.input(inp)
+        
+    def on(self,out):
+        GPIO.output(out, GPIO.LOW)
 
-def on():
-    GPIO.output(17, GPIO.LOW)
-    time.sleep(0.01)
-def off():
-    GPIO.output(17, GPIO.HIGH)
-    time.sleep(0.01)
-def switch():
-    #se a saida estiver acionada desliga
-    if verif_in(17) : on()
+    def off(self,out):
+        GPIO.output(out, GPIO.HIGH)
 
-    else: off()
+    def switch(self,out):
+        #se a saida estiver acionada desliga
+        self.on(out) if GPIO.input(out) == 0 else self.off(out)
+    
+    def state(self):
+        state_list = {'on':[],'off':[]}
+        for x in self.l_out:
+            if GPIO.input(x) == 0:
+                state_list['off'].append(x)
+            else: state_list['on'].append(x)
+        return state_list
 
-def close():
-    GPIO.cleanup()
+    def close(self):
+        GPIO.cleanup()
+    def __str__(self) -> str:
+        return "IO object"
+
--- main.py
+++ main.py
@@ -1,130 +1,97 @@
-import time,datetime,sys,socket,threading,IO
+import time,datetime,socket,threading,IO
 
-botao = IO.start()
+l_inp = [2]
+l_out = [17]
+gpio = IO('BCM',l_inp,l_out)
 #global var
 horas = "6"
 minutos = "0"
-horario_t = "6:0"
 state_timer = True
 
 
 
 def time_clock(var1,var2):
-    global horas, minutos,horario_t
-    horas = var1
-    minutos = var2
-    horario_t = str(var1)+":"+str(var2)
+    global horas, minutos
+    return str(horas)+":"+str(minutos)
 
 def server():
     HOST = ''
     PORT = 50000
-    try:
-        while True:
-            try:
-                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                    s.settimeout(10)
-                    s.bind((HOST,PORT))
-                    s.listen(1)
-                    conn, addr = s.accept()
-                    with conn:
-                        print('connected by: ', addr)
-                        
-                        while True:
+    while True:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.bind((HOST,PORT))
+                s.listen(1)
+                conn, addr = s.accept()
+                print('connected by: ', addr)
+                
+                while True:
+                    data = conn.recv(1024)
+                    data = data.decode('utf-8')
+                    request = data.split(' ')
+                    if request[0] == 'GET':
+                        #send the state of inputs and outputs
+                        if request[1] == 'state':
+                            state_list = gpio.state()
+                            for k,v in state_list.items():
+                                conn.send(k)
+                                conn.recv(1024)
+                                for x in v:
+                                    y = str(x)
+                                    conn.send(y.encode('utf-8'))
+                                    conn.recv(1024)
+                        elif request[1] == 'timer':  
+                            time = time_clock()
+                            conn.send(time.encode('utf-8'))
+                    elif request[0] == 'SET':
+                        #turn on output
+                        if request[1] == "on":
+                            gpio.on(int(request[2]))
+                            conn.send(b'100 ok')
+
+                        #turn off output
+                        elif request[1] == "off":
+                            gpio.off(int(request[2]))
+                            conn.send(b'100 ok')
+
+                        elif request[1] == "timer":
+                            global state_timer
+                            conn.send(b'100 ok')
                             data = conn.recv(1024)
-                            if not data: time.sleep(1)
-                            conn.sendall(data)
-                            #ligar luz
-                            if data.decode("utf-8") == "on":
-                                IO.on()
-                                data = ''
-                            #desligar luz
-                            elif data.decode("utf-8") == "off":
-                                IO.off()
-                                data = ''
-                            elif data.decode("utf-8") == "timer":
-                                global state_timer
-                                conn.send(data)
-                                print(data)
-                                data = ''
-                                time.sleep(0.1)
-                                if state_timer: conn.send(b"1")
-                                else: conn.send(b"0")
-                                while True:
-                                    data = conn.recv(1024)
-                                    if not data: time.sleep(1)
-                                    print(data)
-                                    if data.decode() == "1":
-                                        state_timer = True
-                                        break
-                                    elif data.decode() == "0":
-                                        state_timer = False
-                                        break
-                            # definir timer
-                            elif data.decode("utf-8") == "time":
-                                global horario_t
-                                conn.sendall(horario_t.encode('utf-8'))
-                                while True:
-                                    data = conn.recv(1024)
-                                    if not data: time.sleep(1)
-                                    conn.sendall(data)
-                                    horario = data.decode("utf-8")
-                                    var1,var2 = horario.split(":")
-                                    if var1.isdigit() and var2.isdigit():
-                                        if (int(var1) > 0 and int(var1) < 24) and (int(var2) >= 0 and int(var2) < 60):
-                                            time_clock(var1,var2)
-                                            data = ""
-                                            conn.sendall(b'ok')
-                                            break
-                            elif data.decode("utf-8") == "dsc_off":
-                                s.close()
-                                break
-            except socket.timeout:
-                s.close()
-                continue
-    except KeyboardInterrupt:
-        print("Thread finished -1")
-        s.close()
-        return -1
-    except OSError:
-        print("OSError")
-        s.close()
-        reborn()
-        return -1
-
-def reconect(s):
-    s.bind(('',50000))
-    s.listen(1)
-    conn, addr = s.accept()
-    with conn:
-        print('connected byy: ', addr)
-
-def reborn():
-    global t
-    t = threading.Thread(target=server,args=())
-    t.start()
+                            data = data.decode('utf-8')
+                            request = data.split(' ')
+                            if request[0] == 'TRUE':
+                                state_timer = True
+                                h,m = request[1].split(':')
+                                global horas,minutos
+                                horas = str(int(h))
+                                minutos = str(int(m))
+                            else:
+                                state_timer = False
         
-
-t  = threading.Thread(target=server,args=())
-t.start()
-
-
-try:
+def timmer(gpio,horas,minutos,state_timer):
     while True:
         hora = datetime.datetime.now()
 
         if hora.hour == int(horas) and hora.minute == int(minutos) and state_timer:
-            IO.on()
+            gpio.on()
             time.sleep(60)
-        elif IO.verif_in(2) and botao:
-            botao = False
-            IO.switch()
-        elif not IO.verif_in(2) and not botao:
-            botao = True
-            IO.switch()
-        else: time.sleep(0.02) 
 
-except KeyboardInterrupt:
-    IO.close()
-    t.join()
-    sys.exit(0)
+t0  = threading.Thread(target=server,args=())
+t1  = threading.Thread(target=timmer,args=(gpio,horas,minutos,state_timer))
+t0.start()
+t1.start()
 
+
+while True:
+    ## continue please future vitor
+    if gpio.verif_in(2) and botao:
+        botao = False
+        IO.switch()
+    elif not gpio.verif_in(2) and not botao:
+        botao = True
+        IO.switch()
+    else: time.sleep(0.02) 
+
+
+t.join()
+gpio.close()
