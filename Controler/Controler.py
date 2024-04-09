fe032d6437918c2a49a522325bda9ef8ab391861
Vitor Spader <vitor.h.spader@gmail.com>
2024-04-08 22:18:45 -0300

    fixed some code issues

--- Controler/Controler.py
+++ Controler/Controler.py
@@ -1,21 +1,23 @@
 from Interface import IBoard, IDatabase, IEvent
+import uuid
 
 class Controler:
-    def __init__(self, _board:[IBoard.IBoard], _event:IEvent.IEvent, _database:IDatabase=None):
+    def __init__(self, _board:IBoard.IBoard, _event:IEvent.IEvent, _database:IDatabase=None):
         self.board = _board
         self.event = _event
+        self.id = uuid.uuid4()
 
-    def turn_on(self, _id_board, _id:[int]):
+    def turn_on(self, _id:[int]):
         self.__verify_list__(_id)
-        self.board[_id_board].set_on(_id)
+        self.board.set_on(_id)
 
-    def turn_off(self, _id_board, _id:[int]):
+    def turn_off(self, _id:[int]):
         self.__verify_list__(_id)
-        self.board[_id_board].set_off(_id)
+        self.board.set_off(_id)
 
-    def switch(self, _id_board, _id:[int]):
+    def switch(self, _id:[int]):
         self.__verify_list__(_id)
-        self.board[_id_board].switch(_id)
+        self.board.switch(_id)
 
     async def set_alarm_on(self):
         await self.event.start_event()
@@ -23,13 +25,16 @@ class Controler:
     def set_alarm_off(self, _id):
         pass
 
-    def get_state(self, _id_board, _id):
+    def get_state(self, _id):
         self.__verify_list__(_id)
-        return self.board[_id_board].get_state(_id)
+        return self.board.get_state(_id)
     
     def __verify_list__(self, list_to_verify:[]):
         if list_to_verify is None:
             raise Exception('Invalid parameters of ligths')
+    
+    def get_id(self) -> str:
+        return str(self.id)
 
-    def close(self, _id_board):
-        self.board[_id_board].close()
+    def close(self):
+        self.board.close()
--- Main.py
+++ Main.py
@@ -6,47 +6,44 @@ from Interface import IBoard, IEvent
 from Model import Event, BasicLoger
 import platform
 
-board:IBoard.IBoard
-
 if platform.machine() in ['armv7l','aarch64']:
     from Model import Raspberry
-    board = [Raspberry.Raspberry(_list_input=[2],
+    board = Raspberry.Raspberry(_list_input=[2],
                                  _list_output=[17],
                                  _list_relationship={2:[17]},
-                                 _loger=BasicLoger.BasicLoger('Raspberry','log.txt'))]
+                                 _loger=BasicLoger.BasicLoger('Raspberry','log.txt'))
 else:
     from Model import TestIO
-    board = [TestIO.TestIO(_list_input=[2],_list_output=[17],_list_relationship=[None,[17]])]
+    board = TestIO.TestIO(_list_input=[2],_list_output=[17],_list_relationship=[None,[17]])
 
 controler = Controler.Controler(board,
                                 Event.Event(None, datetime.time(6,0,0),Event.Event.EVERY_DAY,(lambda :board.set_on([17])))) 
 del board
 app = FastAPI()
 
-#Modelos de dados a serem recebidos pela API
-class Light(BaseModel):
-    Id_board: int
-    Id:list#|int
+#Definição de métodos e paths permitidos
+@app.post("/turn_on?q={_id}")
+def request_turn_on(_id:int):
+    return {"Succesful": controler.turn_on(_id=_id)}
 
 #Definição de métodos e paths permitidos
-@app.post("/turn_on")
-def request_turn_on(Light:Light):
-    return {"Succesful": controler.turn_on(_id_board=Light.Id_board, _id=Light.Id)}
+@app.post("/turn_off?q={_id}")
+def request_turn_off(_id:int):
+    return {"Succesful": controler.turn_off(_id=_id)}
 
 #Definição de métodos e paths permitidos
-@app.post("/turn_off")
-def request_turn_off(Light:Light):
-    return {"Succesful": controler.turn_off(_id_board=Light.Id_board, _id=Light.Id)}
+@app.post("/switch?q={_id}")
+def request_refresh_database(_id:int):
+    return {"Succesful": controler.switch(_id=_id)}
 
-#Definição de métodos e paths permitidos
-@app.post("/switch")
-def request_refresh_database(Light:Light):
-    return {"Succesful": controler.switch(_id_board=Light.Id_board, _id=Light.Id)}
+@app.post("/state?q={_id}")
+def request_read_item(_id:int):
+    return {"state":controler.get_state(_id=_id)}
 
-@app.post("/state")
-def request_read_item(Light:Light):
-    return {"state":controler.get_state(_id_board=Light.Id_board, _id=Light.Id)}
+@app.get("/get_id")
+def request_get_id():
+    return controler.get_id()
 
-@app.get("/close/{_id_board}")
-def request_close(_id_board:int):
-    return controler.close(_id_board)
+@app.get("/close")
+def request_close():
+    return controler.close()
