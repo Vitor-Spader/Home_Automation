2e1cc3458f076ae37227ff3b34d50100e0cc9e75
Vitor Spader <vitor.h.spader@gmail.com>
2024-04-08 22:36:44 -0300

    fixed some code issues

--- Model/Raspberry.py
+++ Model/Raspberry.py
@@ -15,7 +15,7 @@ class Raspberry(IBoard.IBoard):
         if _mode == 'BCM': GPIO.setmode(GPIO.BCM)
         else: GPIO.setmode(GPIO.BOARD)
         self.__setup_IO__()
-        self.list_input_state = {id:GPIO.input(id) == 1 for id in self.list_input}
+        self.list_input_state = {id:not GPIO.input(id) == 1 for id in self.list_input}
         self.add_callback(self.callback)
     
     def __setup_IO__(self):
